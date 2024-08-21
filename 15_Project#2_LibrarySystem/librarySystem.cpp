30ee62448390be2d816f26b174be7ec2b73323e5
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2024-08-21 11:56:03 +0300

    15: added 3 functionalities

--- 15_Project#2_LibrarySystem/librarySystem.cpp
+++ 15_Project#2_LibrarySystem/librarySystem.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -25,33 +26,54 @@ bool is_prefix(const string &str, const string &pre) {
   return true;
 }
 
+bool cmp_name(book &a, book &b) { return a.name < b.name; }
+bool cmp_id(book &a, book &b) { return a.id < b.id; }
+
 struct library_books {
   book books[MAX_SIZE];
   int len{};
 
-  void add(string name, int id, int quantity) {
+  void add() {
+    string name;
+    int id, quantity;
+    cout << "Enter book info: id & name & total quantity: ";
+    cin >> id >> name >> quantity;
     books[len].name = name;
     books[len].id = id;
     books[len].quantity = quantity;
     len++;
   }
 
-  void search(string prefix) {
+  void search() {
+    string pre;
+    cout << "Enter book name prefix: ";
+    cin >> pre;
+    for (int i = 0; i < len; i++) {
+      if (is_prefix(books[i].name, pre)) {
+        cout << books[i].name << endl;
+      }
+    }
   }
 
-  void print(string sort) {
-    // by id or name
+  void print(string type = "id") {
+    if (type == "name")
+      sort(books, books + len, cmp_name);
+    else
+      sort(books, books + len, cmp_id);
+    for (int i = 0; i < len; i++) {
+      cout << "id = " << books[i].id << ", name = " << books[i].name
+           << ", quantitiy " << books[i].quantity << ", borrowed "
+           << books[i].borrowed << endl;
+    }
   }
 };
 
 struct library_users {
   user users[MAX_SIZE];
 
-  void add(string name, int id) {
-  }
+  void add(string name, int id) {}
 
-  void print() {
-  }
+  void print() {}
 };
 
 void print_menu() {
@@ -72,9 +94,9 @@ void print_menu() {
 int menu() {
   int choice{-1};
   while (choice == -1) {
-    cout << "\nEnter your choice [1 - 10]: ";
+    cout << "\nEnter your choice [1 - 11]: ";
     cin >> choice;
-    if (1 > choice || choice > 10) {
+    if (1 > choice || choice > 11) {
       cout << "Invalid input. Input must be between 1 and 11\n"
            << "Enter 10 to print the menu. Please try again\n";
       choice = -1;
@@ -84,8 +106,25 @@ int menu() {
 }
 
 void libSystem() {
-  int chioce = menu();
-  return;
+  library_books library;
+  library_users users;
+  print_menu();
+  while (true) {
+    int choice = menu();
+    if (choice == 11)
+      break;
+    else if (choice == 1)
+      library.add();
+    else if (choice == 2)
+      library.search();
+    else if (choice == 4)
+      library.print();
+    else if (choice == 5)
+      library.print("name");
+    else if (choice == 10)
+      print_menu();
+  }
+  cout << "Bye Bye\n";
 }
 
 int main() {
