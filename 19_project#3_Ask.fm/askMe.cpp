0ea6637e3b7530b0a429babaccc53ef2601e6b0f
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2024-09-05 13:19:20 +0300

    19: change structre of program
    - create struct user
    - fix run() while loop
    - enter a newline after each user in users.txt

--- 19_project#3_Ask.fm/askMe.cpp
+++ 19_project#3_Ask.fm/askMe.cpp
@@ -1,24 +1,15 @@
 #include <cstdlib>
 #include <fstream>
+#include <ios>
 #include <iostream>
 #include <string>
 
-struct askme_sys {
-  bool is_logedin{false};
-  std::ofstream users;
+struct user {
+  std::string user_name, name, password, email;
+  int id;
+  bool aq;
 
-  askme_sys() {
-    users.open("users.txt");
-    if (users.fail()) {
-      std::cerr << "Error opennig file.\n";
-      exit(1);
-    }
-  }
-
-  void add_user() {
-    std::string user_name, name, password, email;
-    int id;
-    bool aq;
+  void add(std::ofstream &users) {
     std::cout << "Enter username (nospaces): ";
     std::cin >> user_name;
     std::cout << "Enter password: ";
@@ -30,19 +21,36 @@ struct askme_sys {
     std::cout << "Allow anonymous questions? (0 or 1): ";
     std::cin >> aq;
 
-    users << id << "," << user_name << "," << password << "," << name << ","
-          << email << "," << aq;
+    saveToFile(users);
   }
 
-  int menu(bool is_first) {
+  void saveToFile(std::ofstream &users) {
+    users << id << "," << user_name << "," << password << "," << name << ","
+          << email << "," << aq << "\n";
+  }
+};
+
+struct askme_sys {
+  bool logedin{false};
+  std::ofstream users;
+
+  askme_sys() {
+    users.open("users.txt", std::ios::app);
+    if (users.fail()) {
+      std::cerr << "Error opennig file.\n";
+      exit(1);
+    }
+  }
+
+  int menu(bool logedin) {
     int choice{};
-    if (is_first) {
-      std::cout << "Menu:\n"
+    if (!logedin) {
+      std::cout << "\nMenu:\n"
                 << "\t1: Login\n"
                 << "\t2: Sing up\n";
-      std::cout << "Enter number in range 1 - 2: ";
+      std::cout << "\nEnter number in range 1 - 2: ";
     } else {
-      std::cout << "Menu:\n"
+      std::cout << "\nMenu:\n"
                 << "\t1: Print Questions To Me\n"
                 << "\t2: Print Questions From Me\n"
                 << "\t3: Answer Question\n"
@@ -51,21 +59,24 @@ struct askme_sys {
                 << "\t6: List System Users\n"
                 << "\t7: Feed\n"
                 << "\t8: Logout\n";
-      std::cout << "Enter number in range 1 - 8: ";
+      std::cout << "\nEnter number in range 1 - 8: ";
     }
     std::cin >> choice;
     return choice;
   }
 
   void run() {
-    while (int choice = menu(is_logedin) && choice != 8) {
-      if (choice == 1 && !is_logedin) {
-        add_user();
-        is_logedin = true;
+    int choice;
+    while ((choice = menu(logedin)) && choice != 8) {
+      if (choice == 1 && !logedin) {
+        // user.login();
+      } else if (choice == 2 && !logedin) {
+        user new_user;
+        new_user.add(users);
+        logedin = true;
       }
     }
   }
-
 };
 
 int main() {
