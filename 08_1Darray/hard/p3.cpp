05af62416ec05d9e67bfa643836d7d17629e02a0
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2024-11-14 20:18:14 +0200

    08: revisit p3 form todo list
    08: hw3 hard p3

--- 08_1Darray/hard/p3.cpp
+++ 08_1Darray/hard/p3.cpp
@@ -1,18 +1,31 @@
+// ToDo >> solved after watching solution
 #include <cassert>
 #include <iostream>
 using namespace std;
 
 int main() {
-	const int size = 200;
-	int n;
-	cout << "N(<200): ";
-	cin >> n;
-	assert(n < 200 && n >= 1);
+  const int size = 200;
+  int n;
+  cout << "N(<200): ";
+  cin >> n;
+  assert(n < 200 && n >= 1);
 
-	int arr[size];
-	cout << "numbers 1 by 1: ";
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
+  int arr[size];
+  cout << "numbers 1 by 1: ";
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
 
+  int res = 0;
+  for (int i = 0; i < n; i++) {
+    res++;
+    for (int j = i; j < n - 1; j++) {
+      if (arr[j] <= arr[j + 1])
+        res++;
+      else
+        break;
+    }
+  }
+
+  cout << "\nOutput: " << res << "\n";
 }
