87a9bac0017010c44710e7e8632adb8d492b838a
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2025-04-02 16:36:20 +0200

    31: fuck designing
    TODO

--- 31_Project5_OnlineBookReader/main.cpp
+++ 31_Project5_OnlineBookReader/main.cpp
@@ -3,17 +3,10 @@
  *  I will try implementing it using MVC pattern*/
 #include <iostream>
 #include <string>
+#include <vector>
 
 enum class LoginStatus { LOGED_IN, LOGED_OUT };
 enum class UserStatus { ADMIN, USER };
-
-class UserView {
-public:
-  void printMenu() {}
-  void showProfile() {}
-  void listHistory() {}
-  void listBooks() {}
-};
 
 class UserModel {
 private:
@@ -34,6 +27,34 @@ public:
   void setPassword(const std::string &password) { this->password = password; }
 };
 
+class UserView {
+public:
+  void printMenu() {}
+  void showProfile() {}
+  void listHistory() {}
+  void listBooks() {}
+
+  UserModel inputAddUser() {
+    std::string tmp;
+    UserModel userData;
+
+    std::cout << "Enter username (no spaces): ";
+    std::cin >> tmp;
+    userData.setUsername(tmp);
+    std::cout << "\nEnter password (no spaces): ";
+    std::cin >> tmp;
+    userData.setPassword(tmp);
+    std::cout << "\nEnter name (no spaces): ";
+    std::cin >> tmp;
+    userData.setName(tmp);
+    std::cout << "\nEnter email (no spaces): ";
+    std::cin >> tmp;
+    userData.setEmail(tmp);
+
+    return userData;
+  }
+};
+
 class UserController {
 private:
   UserModel &userModel;
@@ -42,25 +63,54 @@ private:
 public:
   UserController(UserModel &userModel, UserView &userView)
       : userModel(userModel), userView(userView) {}
+
+  void addUser() { userModel = userView.inputAddUser(); }
+};
+
+class UserRepo {
+private:
+  std::vector<UserModel> users;
+
+public:
+  bool add(UserModel &curUser) { users.push_back(curUser); }
+  UserModel *findUser(const std::string &username) {
+    for (UserModel cur : users) {
+      if (cur.getUsername() == username) {
+        return &cur;
+      }
+    }
+  }
 };
 
 class systemManager {
 private:
+  UserRepo userRepo;
+  UserModel currentUser;
   UserView userView;
-  UserModel userModel;
-  UserController userController(UserModel, UserView);
+  UserController userController;
   LoginStatus loginStatus = LoginStatus::LOGED_OUT;
   UserStatus userStatus;
 
 public:
+  systemManager() : userController(currentUser, userView) {}
+
   void run() {
     while (loginStatus == LoginStatus::LOGED_OUT) {
       int choice = startMenu();
+      switch (choice) {
+      case 1:
+      case 2:
+      }
     }
   }
 
+  void handelSingup() {
+    userController.addUser();
+    userRepo.add(currentUser);
+  }
+
   int startMenu() {
-    std::cout << "Menu:\n"
+    std::cout << "\nMenu:\n"
               << "\t 1: Login\n"
               << "\t 2: Signup\n"
               << "\n"
