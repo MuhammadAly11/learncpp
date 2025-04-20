743919e920d43cc8fc0b7ac4fcf9b20d5418035f
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2025-04-20 07:15:32 +0200

    31: updating structure
    - changes in classes
    - some implementation for login functionality

--- 31_Project5_OnlineBookReader/main.cpp
+++ 31_Project5_OnlineBookReader/main.cpp
@@ -8,18 +8,28 @@
 enum class LoginStatus { LOGED_IN, LOGED_OUT };
 enum class UserStatus { ADMIN, USER };
 
-class UserModel {
+std::string UsedUsernameErr = "This username is already used.";
+
+class UserData {
 private:
   std::string name;
   std::string email;
   std::string username;
   std::string password;
+  UserStatus status;
 
 public:
+  UserData() {}
+  UserData(std::string name, std::string email, std::string username,
+           std::string password, UserStatus status = UserStatus::USER)
+      : name(name), email(email), username(username), password(password),
+        status(status) {}
+
   std::string getName() const { return name; }
   std::string getEmail() const { return email; }
   std::string getUsername() const { return username; }
   std::string getPassword() const { return password; }
+  UserStatus getStatus() const { return status; }
 
   void setName(const std::string &name) { this->name = name; }
   void setEmail(const std::string &email) { this->email = email; }
@@ -29,85 +39,26 @@ public:
 
 class UserView {
 public:
-  void printMenu() {}
-  void showProfile() {}
+  int printMenu(UserData &data) {
+    int choice;
+    showProfileLine(data);
+    std::cout << "\nMenu:\n"
+              << "\t 1: View Profile"
+              << "\t 2: List & Select from my reading history"
+              << "\t 3: List & Select from available books"
+              << "\t 4: Logout";
+    return choice;
+  }
+  void showProfileLine(UserData &data) {
+    std::string view;
+    if (data.getStatus() == UserStatus::USER)
+      view = "User";
+    else
+      view = "Admin";
+    std::cout << "\nHello " << data.getUsername() << " | " << view << " View\n";
+  }
   void listHistory() {}
   void listBooks() {}
-
-  UserModel inputAddUser() {
-    std::string tmp;
-    UserModel userData;
-
-    std::cout << "Enter username (no spaces): ";
-    std::cin >> tmp;
-    userData.setUsername(tmp);
-    std::cout << "\nEnter password (no spaces): ";
-    std::cin >> tmp;
-    userData.setPassword(tmp);
-    std::cout << "\nEnter name (no spaces): ";
-    std::cin >> tmp;
-    userData.setName(tmp);
-    std::cout << "\nEnter email (no spaces): ";
-    std::cin >> tmp;
-    userData.setEmail(tmp);
-
-    return userData;
-  }
-};
-
-class UserController {
-private:
-  UserModel &userModel;
-  UserView &userView;
-
-public:
-  UserController(UserModel &userModel, UserView &userView)
-      : userModel(userModel), userView(userView) {}
-
-  void addUser() { userModel = userView.inputAddUser(); }
-};
-
-class UserRepo {
-private:
-  std::vector<UserModel> users;
-
-public:
-  bool add(UserModel &curUser) { users.push_back(curUser); }
-  UserModel *findUser(const std::string &username) {
-    for (UserModel cur : users) {
-      if (cur.getUsername() == username) {
-        return &cur;
-      }
-    }
-  }
-};
-
-class systemManager {
-private:
-  UserRepo userRepo;
-  UserModel currentUser;
-  UserView userView;
-  UserController userController;
-  LoginStatus loginStatus = LoginStatus::LOGED_OUT;
-  UserStatus userStatus;
-
-public:
-  systemManager() : userController(currentUser, userView) {}
-
-  void run() {
-    while (loginStatus == LoginStatus::LOGED_OUT) {
-      int choice = startMenu();
-      switch (choice) {
-      case 1:
-      case 2:
-      }
-    }
-  }
-
-  void handelSingup() {
-    userController.addUser();
-    userRepo.add(currentUser);
-  }
 
   int startMenu() {
     std::cout << "\nMenu:\n"
@@ -120,6 +71,110 @@ public:
     std::cin >> choice;
     return choice;
   }
+
+  void inputAddUser(UserData &user) {
+    std::string tmp;
+
+    std::cout << "Enter username (no spaces): ";
+    std::cin >> tmp;
+    user.setUsername(tmp);
+    std::cout << "Enter password (no spaces): ";
+    std::cin >> tmp;
+    user.setPassword(tmp);
+    std::cout << "Enter name (no spaces): ";
+    std::cin >> tmp;
+    user.setName(tmp);
+    std::cout << "Enter email (no spaces): ";
+    std::cin >> tmp;
+    user.setEmail(tmp);
+  }
+
+  void printError(std::string &err) {
+    std::cout << "\n" << "Error: " << err << "\n";
+  }
+};
+
+class UserModel {
+private:
+  std::vector<UserData> users;
+
+public:
+  bool add(UserData &curUser) {
+    if (findUser(curUser.getUsername()) != nullptr) {
+      return false;
+    }
+    users.push_back(curUser);
+    return true;
+  }
+
+  UserData *findUser(const std::string &username) {
+    for (UserData &cur : users) {
+      if (cur.getUsername() == username) {
+        return &cur;
+      }
+    }
+    return nullptr;
+  }
+};
+
+class UserController {
+private:
+  UserModel userModel;
+  UserView userView;
+
+public:
+  UserController() : userModel() {}
+
+  UserController(UserModel &userModel, UserView &userView)
+      : userModel(userModel), userView(userView) {}
+
+  UserData *addUser() {
+    UserData newUser;
+    while (true) {
+      userView.inputAddUser(newUser);
+      if (userModel.add(newUser)) {
+        return userModel.findUser(newUser.getUsername());
+      } else {
+        userView.printError(UsedUsernameErr);
+      }
+    }
+  }
+
+  int startMenu() { return userView.startMenu(); }
+};
+
+class systemManager {
+private:
+  UserData *currentUser = nullptr;
+  UserController userController;
+  LoginStatus loginStatus = LoginStatus::LOGED_OUT;
+
+public:
+  systemManager() {}
+  ~systemManager() { currentUser = nullptr; }
+
+  void run() {
+    while (true) {
+      if (loginStatus == LoginStatus::LOGED_OUT) {
+        int choice = userController.startMenu();
+        switch (choice) {
+        case 1:
+          handelLogin();
+          break;
+        case 2:
+          handelSingup();
+          break;
+        }
+      }
+    }
+  }
+
+  void handelSingup() {
+    currentUser = userController.addUser();
+    loginStatus = LoginStatus::LOGED_IN;
+  }
+
+  void handelLogin() {}
 };
 
 int main() {
